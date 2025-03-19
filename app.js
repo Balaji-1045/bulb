9068dafb8a1a3436de631c7b5a5802c23b22944f
Balaji-1045 <b43316265@gmail.com>
2025-03-19 16:08:36 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+var on_btn=document.getElementById("on");
+var off_btn=document.getElementById("off");
+var img=document.getElementById("bulb");
+
+function bulb_on(){
+    img.src="https://cdn.pixabay.com/photo/2017/11/08/00/03/bulb-2928829_1280.jpg"
+}
+function bulb_off(){
+    img.src="https://cdn.pixabay.com/photo/2020/06/03/14/28/light-5255121_1280.jpg"
+}
